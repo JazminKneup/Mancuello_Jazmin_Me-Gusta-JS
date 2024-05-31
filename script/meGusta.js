a1bc318502c76738cd33e5c0b05cfd625b1f5d94
jazminkneup <jazmin.kneup@gmail.com>
2024-05-30 20:03:28 -0400

    semana 3

--- script/meGusta.js
+++ script/meGusta.js
@@ -1,6 +1,6 @@
-let likes = [9, 12, 9]; // Initial likes for the posts
+let likes = [9, 12, 9];
 
 function likePost(postId) {
-    likes[postId - 1]++; // Increase the likes for the corresponding post
-    document.getElementById(`likes${postId}`).textContent = `${likes[postId - 1]} likes`; // Update the text
+    likes[postId - 1]++; 
+    document.getElementById(`likes${postId}`).textContent = `${likes[postId - 1]} likes`; 
 }
